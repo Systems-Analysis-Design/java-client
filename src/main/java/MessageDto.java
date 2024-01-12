550b4ca58f6c3e9c0841ac4b0dbbb532aebc9abe
Bozorgmehr Zia <bozorgmehr1381@gmail.com>
2024-01-12 12:59:25 +0330

    Convert MessageDto to record

--- src/main/java/MessageDto.java
+++ src/main/java/MessageDto.java
@@ -1,17 +1,2 @@
-public class MessageDto {
-    private final String key;
-    private final byte[] value;
-
-    public MessageDto(String key, byte[] value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public byte[] getValue() {
-        return value;
-    }
+public record MessageDto(String key, byte[] value) {
 }
